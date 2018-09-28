ccd0c11306eafcb444ca05806f019fcb4fa1917e
Gabriel Coelho Soares <soares@workmail.com>
2018-09-28 11:49:16 -0300

    Adding server listener and the page

---
+++ server.js
@@ -0,0 +1,10 @@
+var Express = require('express');
+var app = Express();
+
+app.get("/", function(req, res){
+    res.render('index.ejs');
+});
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
